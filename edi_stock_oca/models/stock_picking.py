357d3ebd99b4a7f95c270275fd714e7485174333
OriolMForgeFlow <oriol.miranda@forgeflow.com>
2023-10-04 09:08:15 +0200

    [MIG] edi_stock_oca: Migration to 16.0

--- edi_stock_oca/models/stock_picking.py
+++ edi_stock_oca/models/stock_picking.py
@@ -8,7 +8,7 @@ class StockPicking(models.Model):
     _name = "stock.picking"
     _inherit = ["stock.picking", "edi.exchange.consumer.mixin"]
 
-    edi_auto_disabled = fields.Boolean(
+    edi_disable_auto = fields.Boolean(
         states={
             "draft": [("readonly", False)],
             "waiting": [("readonly", False)],
@@ -17,7 +17,20 @@ class StockPicking(models.Model):
         },
     )
 
+    def action_confirm(self):
+        result = super().action_confirm()
+        if self:
+            self._event("on_confirm_picking").notify(self)
+        return result
+
     def _action_done(self):
-        res = super()._action_done()
-        self._event("on_validate").notify(self)
-        return res
+        result = super()._action_done()
+        if self:
+            self._event("on_validate_picking").notify(self)
+        return result
+
+    def action_cancel(self):
+        result = super().action_cancel()
+        if self:
+            self._event("on_cancel_picking").notify(self)
+        return result
