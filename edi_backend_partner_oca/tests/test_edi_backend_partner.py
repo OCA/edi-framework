f48c138fff499b81530a5565cea93390e2727770
OriolMForgeFlow <oriol.miranda@forgeflow.com>
2023-10-26 11:33:36 +0200

    [MIG] edi_backend_partner_oca: Migration to 16.0

--- edi_backend_partner_oca/tests/test_edi_backend_partner.py
+++ edi_backend_partner_oca/tests/test_edi_backend_partner.py
@@ -1,11 +1,7 @@
 # Copyright 2021 ForgeFlow S.L. (https://www.forgeflow.com)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-import logging
-
 from odoo.addons.edi_oca.tests.common import EDIBackendCommonTestCase
-
-_logger = logging.getLogger(__name__)
 
 
 class EDIBackendPartnerTestCase(EDIBackendCommonTestCase):
