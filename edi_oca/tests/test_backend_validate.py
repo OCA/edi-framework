370fc4f49be49b75e5ccb6b78232f15ae7a50a79
Héctor Vi Or <hector.villarreal@forgeflow.com>
2023-11-06 15:55:41 +0100

    [IMP] edi_oca: Show traceback on exception errors

--- edi_oca/tests/test_backend_validate.py
+++ edi_oca/tests/test_backend_validate.py
@@ -62,10 +62,10 @@ class EDIBackendTestValidateCase(EDIBackendCommonComponentRegistryTestCase):
             [
                 {
                     "edi_exchange_state": "validate_error",
-                    "exchange_error": "Data seems wrong!",
                 }
             ],
         )
+        self.assertIn("Data seems wrong!", self.record_in.exchange_error)
 
     def test_generate_validate_record(self):
         self.record_out.write({"edi_exchange_state": "new"})
@@ -87,10 +87,10 @@ class EDIBackendTestValidateCase(EDIBackendCommonComponentRegistryTestCase):
             [
                 {
                     "edi_exchange_state": "validate_error",
-                    "exchange_error": "Data seems wrong!",
                 }
             ],
         )
+        self.assertIn("Data seems wrong!", self.record_out.exchange_error)
 
     def test_validate_record_error_regenerate(self):
         self.record_out.write({"edi_exchange_state": "new"})
