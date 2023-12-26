454179e2e905c035f73c49dd8acb6b49c09e72ed
duongtq <duongtq@trobz.com>
2023-12-26 12:17:40 +0700

    [ADD] edi_partner_oca

---
+++ edi_partner_oca/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
+# Copyright 2024 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "EDI Partners",
+    "summary": """
+       EDI framework configuration and base logic for partners""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/edi-framework",
+    "depends": [
+        "contacts",
+        "edi_oca",
+    ],
+    "data": ["views/partner_views.xml"],
+}
