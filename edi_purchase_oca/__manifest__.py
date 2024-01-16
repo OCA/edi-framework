191d0b2a0722d0ea1adc884c48a14461da23d5b3
John Herholz <j.herholz@deinetuer.de>
2024-01-16 12:41:04 +0100

    [FIX] edi_purchase_oca: wrong version

--- edi_purchase_oca/__manifest__.py
+++ edi_purchase_oca/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "EDI Purchase",
     "summary": """
         Define EDI Configuration for Purchase Orders""",
-    "version": "15.0.1.0.0",
+    "version": "17.0.1.0.0",
     "license": "LGPL-3",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/edi-framework",
