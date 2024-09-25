4f2a31688d255d7a8b81adac9734113fc02e8bf2
JordiMForgeFlow <jordi.masvidal@forgeflow.com>
2024-09-25 16:02:59 +0200

    [FIX] edi_oca: consider parent record when checking access rule

--- edi_oca/tests/test_security.py
+++ edi_oca/tests/test_security.py
@@ -223,3 +223,16 @@ class TestEDIExchangeRecordSecurity(EDIBackendCommonTestCase):
         self.consumer_record.name = "no_rule"
         with self.assertRaisesRegex(AccessError, "doesn't have 'write' access"):
             exchange_record.with_user(self.user).write({"external_identifier": "1234"})
+
+    @mute_logger("odoo.addons.base.models.ir_model")
+    def test_no_group_no_read_child(self):
+        exchange_record = self.create_record()
+        model = self.consumer_record
+        # Create child record without specific model and res_id
+        # It should follow the access rights of the parent
+        child_exchange_record = self.backend.create_record(
+            "test_csv_output", {"parent_id": exchange_record.id}
+        )
+        msg = rf"not allowed to access '{model._description}' \({model._name}\)"
+        with self.assertRaisesRegex(AccessError, msg):
+            child_exchange_record.with_user(self.user).read()
