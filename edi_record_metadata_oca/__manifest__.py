93201c75602fe855bcfa30f25bf3a7144fbe7384
Enric Tobella <enric.tobella@dixmit.com>
2024-10-28 09:45:05 +0100

    [MIG] edi_record_metadata_oca: Migration to 17.0

--- edi_record_metadata_oca/__manifest__.py
+++ edi_record_metadata_oca/__manifest__.py
@@ -7,7 +7,7 @@
     "summary": """
     Allow to store metadata for related records.
     """,
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "development_status": "Alpha",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/edi-framework",
