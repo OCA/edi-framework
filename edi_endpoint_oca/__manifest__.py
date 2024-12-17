02050ca0ba47908b46909248997c1113a5ef5562
JordiMForgeFlow <jordi.masvidal@forgeflow.com>
2024-12-17 12:59:23 +0100

    [MIG] edi_endpoint_oca: Migration to V18

--- edi_endpoint_oca/__manifest__.py
+++ edi_endpoint_oca/__manifest__.py
@@ -7,7 +7,7 @@
     "summary": """
     Base module allowing configuration of custom endpoints for EDI framework.
     """,
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "development_status": "Beta",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/edi-framework",
