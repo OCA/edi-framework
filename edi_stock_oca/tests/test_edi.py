2df651c2ef103896e7d73b854538b4999e67a0f4
Simone Orsi <simahawk@gmail.com>
2025-08-13 17:44:17 +0200

    edi_stock: fix state trigger
    Rollback to action methods override since the state is computed
    but does not pass from writes.

--- edi_stock_oca/tests/test_edi.py
+++ edi_stock_oca/tests/test_edi.py
@@ -32,11 +32,7 @@ class TestProcessComponent(EDIBackendCommonComponentTestCase):
             }
         )
 
-    @mock.patch("odoo.addons.edi_oca.models.edi_backend.EDIBackend._validate_data")
-    @mock.patch("odoo.addons.edi_oca.models.edi_backend.EDIBackend._exchange_generate")
-    @mock.patch("odoo.addons.edi_oca.models.edi_backend.EDIBackend._exchange_send")
-    def test_01_confirm_picking(self, mock_send, mock_generate, mock_validate):
-        mock_generate.return_value = "TRANSFER VALIDATED"
+    def _create_picking(self):
         picking = self.env["stock.picking"].create(
             {
                 "partner_id": self.partner.id,
@@ -56,9 +52,44 @@ class TestProcessComponent(EDIBackendCommonComponentTestCase):
                 "location_dest_id": self.stock_location.id,
             }
         )
-        self.assertFalse(picking.exchange_record_ids)
         move_a.move_line_ids.quantity = 4
-        picking.button_validate()
+        return picking
+
+    def _mock_generate(self, exc_rec):
+        return f"TRANSFER STATE {exc_rec.record.state}"
+
+    @mock.patch("odoo.addons.edi_oca.models.edi_backend.EDIBackend._validate_data")
+    @mock.patch("odoo.addons.edi_oca.models.edi_backend.EDIBackend._exchange_generate")
+    @mock.patch("odoo.addons.edi_oca.models.edi_backend.EDIBackend._exchange_send")
+    def test_picking_cancel_flow(self, mock_send, mock_generate, mock_validate):
+        mock_generate.side_effect = self._mock_generate
+        picking = self._create_picking()
+        self.assertFalse(picking.exchange_record_ids)
+        picking.action_confirm()
+        self.assertEqual(len(picking.exchange_record_ids), 1)
         record1 = picking.exchange_record_ids
         self.assertEqual(record1.type_id, self.exc_type_out)
-        self.assertEqual(record1._get_file_content(), "TRANSFER VALIDATED")
+        self.assertEqual(record1._get_file_content(), "TRANSFER STATE assigned")
+        picking.action_cancel()
+        self.assertEqual(len(picking.exchange_record_ids), 2)
+        record2 = picking.exchange_record_ids - record1
+        self.assertEqual(record2.type_id, self.exc_type_out)
+        self.assertEqual(record2._get_file_content(), "TRANSFER STATE cancel")
+
+    @mock.patch("odoo.addons.edi_oca.models.edi_backend.EDIBackend._validate_data")
+    @mock.patch("odoo.addons.edi_oca.models.edi_backend.EDIBackend._exchange_generate")
+    @mock.patch("odoo.addons.edi_oca.models.edi_backend.EDIBackend._exchange_send")
+    def test_picking_done_flow(self, mock_send, mock_generate, mock_validate):
+        mock_generate.side_effect = self._mock_generate
+        picking = self._create_picking()
+        self.assertFalse(picking.exchange_record_ids)
+        picking.action_confirm()
+        self.assertEqual(len(picking.exchange_record_ids), 1)
+        record1 = picking.exchange_record_ids
+        self.assertEqual(record1.type_id, self.exc_type_out)
+        self.assertEqual(record1._get_file_content(), "TRANSFER STATE assigned")
+        picking.button_validate()
+        self.assertEqual(len(picking.exchange_record_ids), 2)
+        record2 = picking.exchange_record_ids - record1
+        self.assertEqual(record2.type_id, self.exc_type_out)
+        self.assertEqual(record2._get_file_content(), "TRANSFER STATE done")
