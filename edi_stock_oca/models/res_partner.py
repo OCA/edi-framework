733eedf6ba958923dbf7bc70094fd0272caa1894
Simone Orsi <simahawk@gmail.com>
2025-08-14 08:36:34 +0200

    edi_stock: fix conf field label

--- edi_stock_oca/models/res_partner.py
+++ edi_stock_oca/models/res_partner.py
@@ -9,7 +9,7 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     edi_stock_picking_conf_ids = fields.Many2many(
-        string="EDI sale configuration",
+        string="EDI stock configuration",
         comodel_name="edi.configuration",
         relation="res_partner_edi_stock_picking_configuration_rel",
         column1="partner_id",
