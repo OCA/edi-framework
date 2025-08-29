8e93183e53504f9bb28fb82d3e5e405911eeb8ec
Enric Tobella <enric.tobella@dixmit.com>
2025-08-29 19:19:42 +0200

    [ADD] edi_account_core_oca: Module that includes the integration between edi and account without components

---
+++ edi_account_core_oca/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2020 Dixmit
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Edi Account",
+    "summary": """
+        Define EDI Configuration for Account Moves""",
+    "version": "18.0.1.1.0",
+    "license": "LGPL-3",
+    "author": "Dixmit,Odoo Community Association (OCA)",
+    "maintainers": ["etobella"],
+    "development_status": "Beta",
+    "website": "https://github.com/OCA/edi-framework",
+    "depends": ["account", "edi_core_oca"],
+    "pre_init_hook": "pre_init_hook",
+    "data": [
+        "views/account_journal.xml",
+        "views/res_partner.xml",
+        "views/account_move.xml",
+        "views/edi_exchange_record.xml",
+    ],
+    "demo": [],
+}
