6178337208e3d8776a5c21250aa9e02ed1902d2b
JordiMForgeFlow <jordi.masvidal@forgeflow.com>
2026-04-10 15:39:19 +0200

    [MIG] edi_account_oca: migration to V19

--- edi_account_oca/__manifest__.py
+++ edi_account_oca/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Account Listeners",
     "summary": """
         Define some component listeners for Account Moves""",
-    "version": "18.0.1.1.1",
+    "version": "19.0.1.0.0",
     "license": "LGPL-3",
     "author": "Dixmit,Odoo Community Association (OCA)",
     "maintainers": ["etobella"],
