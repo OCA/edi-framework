921bec31d5f018b6be6e8b8660f1e723edce3e5b
Hadrien Huvelle <hhu@baboum.be>
2026-06-11 12:29:55 +0200

    [IMP] edi_core_oca: Improve result message

--- edi_core_oca/models/edi_backend.py
+++ edi_core_oca/models/edi_backend.py
@@ -16,6 +16,7 @@ from odoo import exceptions, fields, models
 from odoo.exceptions import UserError
 
 from ..exceptions import EDINotImplementedError, EDIValidationError
+from ..utils import EdiExchangeReturn
 
 _logger = logging.getLogger(__name__)
 
@@ -242,7 +243,7 @@ class EDIBackend(models.Model):
         message = None
         res = ""
         try:
-            self._exchange_send(exchange_record)
+            send_result = self._exchange_send(exchange_record)
             _logger.debug("%s sent", exchange_record.identifier)
         except self._send_retryable_exceptions() as err:
             traceback = _get_exception_traceback()
@@ -267,8 +268,10 @@ class EDIBackend(models.Model):
             res = "__sql_error__"
             raise
         else:
-            # TODO: maybe the send handler should return desired message and state
-            message = exchange_record._exchange_status_message("send_ok")
+            if isinstance(send_result, EdiExchangeReturn):
+                message = str(send_result)
+            else:
+                message = exchange_record._exchange_status_message("send_ok")
             error = traceback = None
             state = (
                 "output_sent_and_processed"
--- edi_core_oca/utils.py
+++ edi_core_oca/utils.py
@@ -5,6 +5,32 @@
 import hashlib
 
 
+class EdiExchangeReturn:
+    """Structured return value for EDI exchange send operations.
+
+    Handlers can return an instance of this class instead of a plain string
+    so that callers can distinguish structured results from legacy str returns
+    and display rich information (e.g. HTTP status code + diff) as the job
+    result.
+
+    Legacy str returns are still supported: callers fall back to
+    ``exchange_record._exchange_status_message("send_ok")`` when the result
+    is not an ``EdiExchangeReturn`` instance.
+    """
+
+    def __init__(self, message, code=None):
+        self.message = message
+        self.code = code
+
+    def __str__(self):
+        if self.code is not None:
+            return f"[{self.code}] {self.message}"
+        return self.message
+
+    def __repr__(self):
+        return f"EdiExchangeReturn(code={self.code!r}, message={self.message!r})"
+
+
 def normalize_string(cls, a_string, sep="_"):
     """Normalize given string, replace dashes with given separator."""
     return cls.env["ir.http"]._slugify(a_string).replace("-", sep)
