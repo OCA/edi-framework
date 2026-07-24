598621c2b9fe55ca112040069afde7e80e79e600
Akim Juillerat <akim.juillerat@camptocamp.com>
2026-07-24 20:34:04 +0200

    [IMP] edi_queue_oca: update exc record state on failed jobs
    Rely on new on fail hooks from the queue job module.

--- edi_queue_oca/models/edi_exchange_record.py
+++ edi_queue_oca/models/edi_exchange_record.py
@@ -91,3 +91,25 @@ class EdiExchangeRecord(models.Model):
         # Raise prio to max to send the record out as fast as possible.
         job1.on_done(self.delayable(priority=0).action_exchange_send())
         job1.delay()
+
+    def _job_on_fail_generate(self, **kw):
+        return self._job_on_fail_update("validate_error", **kw)
+
+    def _job_on_fail_send(self, **kw):
+        return self._job_on_fail_update("output_error_on_send", **kw)
+
+    def _job_on_fail_receive(self, **kw):
+        return self._job_on_fail_update("input_receive_error", **kw)
+
+    def _job_on_fail_process(self, **kw):
+        return self._job_on_fail_update("input_processed_error", **kw)
+
+    def _job_on_fail_update(self, failed_state, **kw):
+        self.ensure_one()
+        self.write(
+            {
+                "edi_exchange_state": failed_state,
+                "exchange_error": ": ".join([kw["exc_name"], kw["exc_message"]]),
+                "exchange_error_traceback": kw["exc_info"],
+            }
+        )
--- edi_queue_oca/tests/test_backend_jobs.py
+++ edi_queue_oca/tests/test_backend_jobs.py
@@ -159,3 +159,24 @@ class EDIBackendTestJobsCase(EDIBackendCommonTestCase, JobMixin):
         # Check related jobs
         record.invalidate_recordset()
         self.assertEqual(created, self._get_related_jobs(record))
+
+    def test_on_fail_job(self):
+        vals = {
+            "model": self.partner._name,
+            "res_id": self.partner.id,
+        }
+        record = self.backend.create_record("test_csv_output", vals)
+        self.assertEqual(record.edi_exchange_state, "new")
+        job = record.action_exchange_generate()
+        exc_vals = {
+            "exc_info": "Dummy traceback",
+            "exc_name": "Dummy exception",
+            "exc_message": "Dummy message",
+        }
+        job.on_fail(exc_vals)
+        self.assertEqual(record.edi_exchange_state, "validate_error")
+        self.assertEqual(
+            record.exchange_error,
+            ": ".join([exc_vals["exc_name"], exc_vals["exc_message"]]),
+        )
+        self.assertEqual(record.exchange_error_traceback, exc_vals["exc_info"])
