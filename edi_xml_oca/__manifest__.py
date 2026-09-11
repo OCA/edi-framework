c76f14f53dd13fd3a6f6acae2ed0a203f0cb774d
François Honoré <francois.honore@acsone.eu>
2026-09-11 07:22:13 +0200

    [UPD] edi_xml_oca: change development_status from Alpha to Beta

--- edi_xml_oca/__manifest__.py
+++ edi_xml_oca/__manifest__.py
@@ -8,7 +8,7 @@
         Base module for EDI exchange using XML files.
     """,
     "version": "19.0.1.0.0",
-    "development_status": "Alpha",
+    "development_status": "Beta",
     "license": "LGPL-3",
     "author": "ACSONE,Camptocamp,Odoo Community Association (OCA)",
     "maintainers": ["simahawk"],
